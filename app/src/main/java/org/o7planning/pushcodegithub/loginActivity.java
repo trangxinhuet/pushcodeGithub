c53c6bf014573f374c108cdf9b445d6d8db5391c
trangxinhuet <trangxinhbg1993@gmail.com>
2017-05-12 13:15:48 +0700

    commit change

---
+++ app/src/main/java/org/o7planning/pushcodegithub/loginActivity.java
@@ -0,0 +1,10 @@
+package org.o7planning.pushcodegithub;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Trang Ken on 5/12/2017.
+ */
+
+public class loginActivity extends AppCompatActivity {
+}
